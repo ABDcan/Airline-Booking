fcaf0e72d66218f23de3897248e4d6b9c6263587
Abhishe <lcs2021036@iiitl.ac.in>
2024-12-15 15:26:23 +0530

    setup basic bodypaser

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const bodyParer = require('body-parser');
+
 const {PORT} =  require('./config/serverConfig')
 const setupAndStartServer = async() =>{
 //  create the express object
   const app = express();
+  app.use(bodyParer.json());
+  app.use(bodyParer.urlencoded({extended:true}))
   app.listen(3000,()=>{
     console.log(`server started at ${PORT}`)
     // console.log(process);
