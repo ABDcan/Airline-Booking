04dd5be83ecebd670653ddfa11710fbca15ee6f5
Abhishe <lcs2021036@iiitl.ac.in>
2024-12-16 18:46:04 +0530

    minor fixes

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -35,11 +35,17 @@ class CityRepository{
     }
     async updateCity(cityId,data){
         try{
-            const city = await City.update(data,{
-                where:{
-                    id:cityId
-                } 
-            });
+            // This one also works but it will not return us the updated object
+            // const city = await City.update(data,{
+            //     where:{
+            //         id:cityId
+            //     } 
+            // });
+            // return city;
+
+            const city =  await City.findByPk(cityId);
+            city.name = data.name;
+            await city.save();
             return city;
         }catch{
             console.log("Something went wrong in the repository layer");
